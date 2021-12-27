508f5069ce6ce250fb0ab6ecf0719d1037d44da3
kokojer <gordei80z34@gmail.com>
2021-12-27 13:46:13 +0300

    added 2 files

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputFirst = document.querySelector('input:first-of-type');
+const inputs = document.querySelectorAll('input:not(:first-of-type)');
+const p = document.querySelector('p')
+// const button = document.querySelector('button')
+
+
+
+inputFirst.addEventListener('blur', () =>{
+        sum = 0;
+        p.textContent = new Date().getFullYear() - inputFirst.value;
+        console.log();
+
+
+        
+    })
